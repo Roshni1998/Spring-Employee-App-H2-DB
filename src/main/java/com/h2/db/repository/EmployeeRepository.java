2c72d83ef509c0dfb072e89759cb73f466e2f254
Roshni <roshni.mali@bridgelabz.com>
2022-02-05 00:07:46 +0530

    Added: Employee Payroll App using Thymleaf and H2 Database

---
+++ src/main/java/com/h2/db/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.h2.db.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.h2.db.model.EmployeeEntity;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<EmployeeEntity, Long> {
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO 
+	EMPLOYEE_TABLE (first_name, last_name, email)
+VALUES
+  	('Arjun', 'Malhotra', 'arjun@gmail.com'),
+  	('Rohit', 'Sharma', 'rohit@gmail.com'),
+  	('Kavita', 'Sen', 'kavita@gmail.com');
+  	
